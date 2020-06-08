5f12beb1e24c0092ee2a95a339139a43a172f6db
Richard <richardsp.dev@gmail.com>
2020-06-08 19:19:43 -0300

    style: rename variables for consistency

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -1,43 +1,45 @@
-function populateUF() {
-    const ufSelect = document.querySelector('select[name=uf]')
+function populateStates() {
+    const stateSelect = document.querySelector('select[id=state]')
 
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome")
         .then(res => res.json())
         .then(states => {
             for (let state of states) {
-                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+                stateSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
             }
         })
 }
 
-populateUF()
+populateStates()
 
 function getCities(event) {
-    const citySelect = document.querySelector('select[name=city]')
-    
+    // Store State name in the hidden input
     const stateInput = document.querySelector('input[name=state]')
-    const ufValue = event.target.value
-
     const indexOfSelectedState = event.target.selectedIndex
     stateInput.value = event.target.options[indexOfSelectedState].text
 
+    const stateId = event.target.value
+    const citySelect = document.querySelector('select[name=city]')
+    
     citySelect.innerHTML = '<option value="">Selecione a cidade</option>'
     citySelect.disabled = true
 
-    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios?orderBy=nome`)
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${stateId}/municipios?orderBy=nome`)
         .then(res => res.json())
         .then(cities => {
-            let cityOptions = ''
+            let cityOptions = citySelect.innerHTML
+            
             for (let city of cities) {
                 cityOptions += `<option value="${city.nome}">${city.nome}</option>`
             }
+
             citySelect.innerHTML = cityOptions
             citySelect.disabled = false
         })
 }
 
 document
-    .querySelector('select[name=uf]')
+    .querySelector('select[id=state]')
     .addEventListener('change', getCities)
 
 document.querySelectorAll('.items-grid li').forEach(item => {
