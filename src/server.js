richardspacheco/nlw-1

commit 106b024da38d0e8725918be9e11f1fa1e810f91d

feat: include search by state

src/server.js
@@ -62,7 +62,7 @@ server.get('/search', (req, res) => {
     if (search === '') {
         sql = `SELECT * FROM places`
     } else {
-        sql = `SELECT * FROM places WHERE city LIKE '%${search}%'`
+        sql = `SELECT * FROM places WHERE city OR state LIKE '%${search}%'`
     }
 
     db.all(sql, function (err, rows) {

public/styles/modal.css
@@ -40,13 +40,6 @@
     line-height: 42px;
 }
 
-#modal form label {
-    font-size: 14px;
-    line-height: 16px;
-    display: block;
-    margin-bottom: 8px;
-}
-
 #modal form .field {
     display: flex;
 }

src/views/partials/modal.html
@@ -7,9 +7,8 @@
             </a>
         </header>
         <form action="/search">
-            <label for="search">Cidade</label> 
             <div class="search field">
-                <input type="text" name="search" placeholder="Pesquise por cidade">
+                <input type="text" name="search" placeholder="Pesquise por cidade ou estado">
                 <button>
                     <img src="/assets/search.svg" alt="Buscar">
                 </button>

commit 07beaf61ada4cb16d0d51a9f842f8504fe04230a

fix: correct SQL expression for search results
This fixes bug introduced in 106b024da38d0e8725918be9e11f1fa1e810f91d

src/server.js
@@ -62,7 +62,7 @@ server.get('/search', (req, res) => {
     if (search === '') {
         sql = `SELECT * FROM places`
     } else {
-        sql = `SELECT * FROM places WHERE city OR state LIKE '%${search}%'`
+        sql = `SELECT * FROM places WHERE city LIKE '%${search}%' OR state LIKE '%${search}%'`
     }
 
     db.all(sql, function (err, rows) {